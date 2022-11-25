3f49e7670b738194437e1fa318392d4ac600c497
Teste <teste@gmail.com>
2022-11-25 08:16:34 -0300

    :sparkles: feat: Livro Repository

---
+++ src/main/java/br/com/davisadoraADS/LibraryAPI/repository/LivroRepository.java
@@ -0,0 +1,20 @@
+package br.com.davisadoraADS.LibraryAPI.repository;
+
+import br.com.davisadoraADS.LibraryAPI.model.Livro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+    Optional<Livro> findById(long id);
+
+    public Page<Livro> findAll(Pageable page);
+
+    List<Livro> findByNameContainsIgnoreCaseOrderByName(String name);
+
+}
